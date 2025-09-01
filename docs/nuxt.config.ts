870c728d9c13c170ea5a95b98931b8d5fbc60f6b
oyedejioyewole <57059138+oyedejioyewole@users.noreply.github.com>
2025-09-01 22:21:06 +0100

    docs: 🖌  Add `site:url` key for nuxt-og-image in theme layer.

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -2,5 +2,8 @@ export default defineNuxtConfig({
   extends: [import.meta.env.THEME_DIRECTORY ?? ['github:oyedejioyewole/nuxt-pastel-docs', { install: true }]],
   modules: ['../src/module'],
   devtools: { enabled: true },
+  site: {
+    url: 'https://nuxt-useless-blobs.vercel.app',
+  },
   compatibilityDate: '2025-07-15',
 })
